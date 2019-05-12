5c2d9f859981633e608cfdd77bba62e5c258f3ef
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-12 01:21:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,13 @@
 class School
   attr_accessor :roster, :grade
   
-  @roster[grade] = []
-  
   def initialize(roster)
     @roster = {}
   end 
+  
+  def grade= (grade)
+    @roster[grade] = []
+  end  
   
   def add_student(student,grade)
     @roster[grade] << student
